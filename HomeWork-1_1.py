6738623886b72226898d7621c3907757cbb4026a
Eugene <leugene7@gmail.com>
2022-11-22 22:28:31 +0500

    Seminar 1 task 1

---
+++ HomeWork-1_1.py
@@ -0,0 +1,9 @@
+'''
+Поработайте с переменными, создайте несколько, выведите на экран, запросите у
+пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+'''
+a = int(input('Input number: a = '))
+b = int(input('Input number two: b =  '))
+print(f'Summ of numbers a and b is {a + b}')
+name = input('What is youre name? \n :')
+print(f'Hello {name}!')
